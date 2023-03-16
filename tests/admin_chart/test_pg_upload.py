49365b7abe97eec7e0a29e8679aaa840d646ba69
Nick Zaaijer <n.zaaijer@amsterdam.nl>
2023-03-16 09:51:04 +0100

    remove created file when running tests

--- tests/admin_chart/test_pg_upload.py
+++ tests/admin_chart/test_pg_upload.py
@@ -10,32 +10,34 @@ from admin_chart.management.commands.pgdump import Command
 class TestPgdumpCommand:
 
     def test_pg_dump_create_file(self):
-        database = settings.DATABASES['default']
+        database = settings.DATABASES["default"]
         succesfull = Command().start_dump(database)
         assert succesfull
         assert os.path.isfile("meetbouten.dump")
-        os.remove('meetbouten.dump')
+        os.remove("meetbouten.dump")
 
     def test_pg_dump_upload(self):
-        f = open('meetbouten.dump', 'w')
+        f = open("meetbouten.dump", "w")
         f.close()
-        name = Command().upload_to_blob()
-        assert "pg_dump/meetbouten" in name
-        os.remove('meetbouten.dump')
+        Command().upload_to_blob()
+        assert os.path.isfile("/src/media/pg_dump/meetbouten.dump")
+        os.remove("meetbouten.dump")
+        os.remove("/src/media/pg_dump/meetbouten.dump")
 
     def test_pg_dump_remove(self):
-        f = open('meetbouten.dump',  'w')
+        f = open("meetbouten.dump",  "w")
         f.close()
         Command().remove_dump()
-        assert not os.path.isfile('meetbouten.dump')
+        assert not os.path.isfile("meetbouten.dump")
 
     def test_pg_dump(self):
         """
         Check the whole happy flow
         """
         try:
-            call_command('pgdump')
+            call_command("pgdump")
         except Exception as e:
             print(e)
             assert False
-
+        finally:
+            os.remove("/src/media/pg_dump/meetbouten.dump")
