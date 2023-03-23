74fe94406616a64a80591987d645bb7de88df8f2
jjbeekman <jonathanjeroen@gmail.com>
2023-03-23 11:31:40 +0100

    Initiating id sequences on database after data load

--- tests/metingen/management/commands/test_load_csv_data.py
+++ tests/metingen/management/commands/test_load_csv_data.py
@@ -1,7 +1,8 @@
 import pytest
 from django.core.management import call_command
+from django.db import connection
 
-from metingen.models import Meting, MetingHerzien, MetingReferentiepunt, MetRefPuntenHerz
+from metingen.models import Meting, MetingHerzien, MetingReferentiepunt, MetRefPuntenHerz, Hoogtepunt, Grondslagpunt
 
 
 @pytest.mark.migration
@@ -12,3 +13,13 @@ def test_load_csv_data():
     assert len(MetingHerzien.objects.all()) > 0
     assert len(MetingReferentiepunt.objects.all()) > 0
     assert len(MetRefPuntenHerz.objects.all()) > 0
+    assert len(Hoogtepunt.objects.all()) > 0
+    assert len(Grondslagpunt.objects.all()) > 0
+
+    # Check if sequence is set correctly
+    for model in [MetingHerzien, Meting, Hoogtepunt, Grondslagpunt]:
+        with connection.cursor() as cursor:
+            query = f"""select nextval('{model._meta.db_table}_id_seq'::regclass)"""
+            cursor.execute(query)
+            value = cursor.fetchall()
+            assert value[0][0] > 999
